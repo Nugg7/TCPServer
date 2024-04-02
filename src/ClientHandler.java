c0033ccb3492d63ad4ca73b00f5aee98e0de24cd
nuggs <steven.guevarra.1@gmail.com>
2024-04-02 15:39:39 +0200

    added comments to explain code

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -16,32 +16,29 @@ public class ClientHandler implements Runnable {
     }
 
     protected void readLoop(PrintWriter out, BufferedReader in){
+        //function to read constantly the responses from client
         JSONManagerServer json = new JSONManagerServer();
         String s;
         JSONObject message = null;
         try {
             while ((s = in.readLine()) != null) {
                 try{
-                    message = json.parse(s);
+                    message = json.parse(s); //parses the json message of the client from String to JSONObject
                 }catch (ParseException e){
                     System.out.println("error parsing");
                 }
-                if (message.get("message").equals("")){
+                if (message.get("message").equals("")){ //if it's the first connection of the client the message will be empty and the server will save the client in the JSON file
                     clientUsername = (String) message.get("username");
                     clientUuid = UUID.fromString((String) message.get("UUID"));
                     System.out.println("connected : " + clientUsername + "\nUUID : " + clientUuid);
                 }
                 else{
-                    System.out.println("Received: " + message);
-                }
-                if (s.equalsIgnoreCase("exit")) {
-                    break;
+                    System.out.println("Received: " + message); //prints out the message of the client
                 }
             }
-
-            System.out.println("Client exited");
+            System.out.println("Client exited"); //prints out when client exits
         } catch (IOException e) {
-            System.out.println("client exited");
+            System.out.println("client exited"); //prints out when client exits
             //e.printStackTrace();
         }
     }
@@ -52,7 +49,7 @@ public class ClientHandler implements Runnable {
         try (
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
-        ) {
+        ) { //code to execute when client connects
             readLoop(out, in);
         } catch (IOException e) {
             e.printStackTrace();
--- src/Server.java
+++ src/Server.java
@@ -7,28 +7,28 @@ import java.net.Socket;
 
 
 public class Server {
-    static int portNumber = 1234;
+    static int portNumber = 1234; // port number of the server
 
     public static void main(String[] args) {
 
         ServerSocket serverSocket = null;
 
         try {
-            serverSocket = new ServerSocket(portNumber);
+            serverSocket = new ServerSocket(portNumber); //creates server socket with the port number specified
             System.out.println("Server started!");
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Accepted connection from: " + clientSocket.getInetAddress().getHostAddress());
-                ClientHandler clientHandler = new ClientHandler(clientSocket);
-                Thread thread = new Thread(clientHandler);
-                thread.start();
+                Socket clientSocket = serverSocket.accept(); //accepts client conntection from server socket
+                System.out.println("Accepted connection from: " + clientSocket.getInetAddress().getHostAddress()); //prints ip of the client which connected
+                ClientHandler clientHandler = new ClientHandler(clientSocket); //creates client handler
+                Thread thread = new Thread(clientHandler); //creates thread for the client that connected
+                thread.start(); //starts the thread
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
                 if (serverSocket != null)
-                    serverSocket.close();
+                    serverSocket.close(); //closes the server socket
             } catch (IOException e) {
                 e.printStackTrace();
             }
